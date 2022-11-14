dfd6614a838261750aa4d5242473056492073831
gankhulugdev <ihcrew_hulguu@yahoo.com>
2022-11-13 21:21:30 -0600

    using effect hook

--- src/memory-game/memory-game.js
+++ src/memory-game/memory-game.js
@@ -1,5 +1,5 @@
 import "./memory-game.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import icon0 from "./assets/card.png";
 import icon1 from "./assets/clock.png";
@@ -10,61 +10,58 @@ import icon5 from "./assets/hammer.png";
 import icon6 from "./assets/heart.png";
 import icon7 from "./assets/key.png";
 import icon8 from "./assets/magnet.png";
-import startButton from "./assets/start_button.png"
+import startButton from "./assets/start_button.png";
 
 const MemoryGame = () => {
   const randomize = (arr) => arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
 
   // const [randomizedCards, setRandomizedCards] = useState([...cards, ...cards]); for testing purpose
-  const [randomizedCards, setRandomizedCards] = useState([...randomize([...cards,...cards])]);
+  const [randomizedCards, setRandomizedCards] = useState(
+    randomize([...cards, ...cards])
+  );
   const [flippedCard, setFlippedCard] = useState({});
   const [matchedCards, setMatchedCards] = useState({});
   const [currentPlayer, setCurrentPlayer] = useState("playerOne");
   const [score, setScore] = useState({ playerOne: 0, playerTwo: 0 });
   const [isWin, setIsWin] = useState(false);
 
-  const flipCard = (card, id) => {
-    const tempFlippedCard = {
-      ...flippedCard,
-      [id]: { ...card, player: currentPlayer },
-    };
-
-    if (Object.keys(tempFlippedCard).length === 2) {
-      const [first, second] = Object.keys(tempFlippedCard);
+  useEffect(() => {
+    if (Object.keys(flippedCard).length === 2) {
+      const [first, second] = Object.keys(flippedCard);
       // check matching
-      if (tempFlippedCard[first].name === tempFlippedCard[second].name) {
-        const tempMatchedCards = {
-          ...matchedCards,
-          [first]: tempFlippedCard[first],
-          [second]: tempFlippedCard[second],
-        };
-
-        setMatchedCards(tempMatchedCards);
+      if (flippedCard[first].name === flippedCard[second].name) {
+        setMatchedCards((currentState) => ({
+          ...currentState,
+          [first]: flippedCard[first],
+          [second]: flippedCard[second],
+        }));
         setScore((currentState) => ({
           ...currentState,
           [currentPlayer]: currentState[currentPlayer] + 1,
         }));
 
-        if (Object.keys(tempMatchedCards).length === 16) {
-          // win
-          setIsWin(true);
-        }
         setFlippedCard({});
       } else {
-        setFlippedCard(tempFlippedCard);
         setCurrentPlayer((currentState) =>
           currentState === "playerOne" ? "playerTwo" : "playerOne"
         );
-        setTimeout(() => setFlippedCard({}), 2000);
+        setTimeout(() => setFlippedCard({}), 1000);
       }
-    } else {
-      setFlippedCard(tempFlippedCard);
     }
-  };
+  }, [flippedCard]);
+
+  useEffect(() => {
+    if (Object.keys(matchedCards).length === 16) {
+      // win
+      setIsWin(true);
+    }
+  }, [matchedCards]);
 
   return (
     <div className="game">
-      <div className="player">  {isWin
+      <div className="player">
+        {" "}
+        {isWin
           ? `${
               score.playerOne > score.playerTwo
                 ? "Player One wins"
@@ -72,7 +69,8 @@ const MemoryGame = () => {
                 ? "Player two wins"
                 : "Draw"
             }`
-          : `Turn ${currentPlayer === 'playerOne' ? 'Player I' :'Player II'}`}</div>
+          : `Turn ${currentPlayer === "playerOne" ? "Player I" : "Player II"}`}
+      </div>
       <div className="cards-container">
         <div className="score">Player I : {score.playerOne}</div>
         <div className="cards">
@@ -84,10 +82,12 @@ const MemoryGame = () => {
                 onClick={() => {
                   Object.keys(flippedCard).length <= 1 &&
                     !matchedCards[idx] &&
-                    flipCard(card, idx);
+                    setFlippedCard((currState) => ({
+                      ...currState,
+                      [idx]: { ...card, player: currentPlayer },
+                    }));
                 }}
               >
-                {/* <Card /> */}
                 {flippedCard[idx] || matchedCards[idx] ? (
                   <img
                     src={`${
@@ -107,7 +107,8 @@ const MemoryGame = () => {
         <div className="score">Player II : {score.playerTwo}</div>
       </div>
 
-      <button className="start-btn"
+      <button
+        className="start-btn"
         onClick={() => {
           setScore({ playerOne: 0, playerTwo: 0 });
           setIsWin(false);
@@ -115,17 +116,13 @@ const MemoryGame = () => {
           setMatchedCards({});
           setFlippedCard({});
 
-          // setRandomizedCards([...randomize(cards), ...randomize(cards)])
           setRandomizedCards((currentState) => {
             return [...randomize(currentState)];
           });
         }}
       >
-        <img className="start-btn" src={startButton}/>
+        <img className="start-btn" src={startButton} />
       </button>
-      <div className="win">
-      
-      </div>
     </div>
   );
 };

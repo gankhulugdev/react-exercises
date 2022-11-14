0bb568540e63b0da0ae362af927314176323fb4b
gankhulugdev <ihcrew_hulguu@yahoo.com>
2022-11-13 22:28:55 -0600

    added TodoList

--- src/controlled-comp/index.js
+++ src/controlled-comp/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ImBin2, ImPencil } from "react-icons/im";
 import { BiSave } from "react-icons/bi";
 import { AiOutlineCheckCircle, AiOutlineFileAdd } from "react-icons/ai";
@@ -6,63 +6,86 @@ import "./style.css";
 
 export default function ToDoList() {
   // state that saves the todo items
-  const [todoItems, setTodoItems] = useState([]);
+  const [todoItems, setTodoItems] = useState(
+    localStorage.getItem("my-to-do-items")
+      ? JSON.parse(localStorage.getItem("my-to-do-items"))
+      : []
+  );
   // state that saves the value of the input field
   const [inputField, setInputField] = useState("");
   // state that saves if it's a new todo or edit
   const [editItemIdx, setEditItemIdx] = useState(-1);
-  // state that 
-  const [isCompleted, setIsCompleted] = useState('')
+  // state that
+  const [isCompleted, setIsCompleted] = useState("all");
   // search field
-  const [searchField, setSearchField] = useState('')
-
+  const [searchField, setSearchField] = useState("");
 
   // function that changes the "isCompleted" status
-  const complete = (itemIdx) => {
-    console.log(itemIdx)
+  const complete = (id) => {
     setTodoItems((currentState) =>
-      currentState.map((item, id) =>
-        itemIdx === id ? { ...item, isCompleted: !item.isCompleted } : item
+      currentState.map(item =>
+        id === item.id ? { ...item, isCompleted: !item.isCompleted } : item
       )
     );
   };
   // function that deletes the item from the list (i.e. update the todo items state)
-  const deleteItem = (itemIdx) => {
+  const deleteItem = (id) => {
     setTodoItems((currentState) =>
-      currentState.filter((item, itemId) => itemId !== itemIdx)
+      currentState.filter((item) => item.id !== id)
     );
   };
-  //  edit function - better to do it at the end
-  const editItem = (itemIdx) => {
-    setEditItemIdx(itemIdx);
-    setInputField(todoItems[itemIdx].title);
+  //  edit function
+  const editItem = (item) => {
+    setEditItemIdx(item.id);
+    setInputField(item.title);
   };
 
   // submit function, that takes the input field value and add it to the todo items state
 
   const submit = (e) => {
-    if(editItemIdx=== -1) {
-      setTodoItems((currentState)=> {
-        return [...currentState, {
-          title: inputField,
-          createdDate: new Date().toUTCString(),
-          isCompleted: false,
-        }]
-      })
-    }else{
-      setTodoItems((currentState)=>{
-        return currentState.map((item,id)=>{
-          return id === editItemIdx ? { ...item, title: inputField } : item;
-        })
-      })
+    if (editItemIdx === -1) {
+      setTodoItems((currentState) => {
+        return [
+          ...currentState,
+          {
+            id: Math.floor(Math.random() * 100000),
+            title: inputField,
+            createdDate: new Date().toUTCString(),
+            isCompleted: false,
+          },
+        ];
+      });
+    } else {
+      setTodoItems((currentState) => {
+        return currentState.map((item) => {
+          return item.id === editItemIdx
+            ? { ...item, title: inputField }
+            : item;
+        });
+      });
     }
     cancelEdit();
   };
 
   const cancelEdit = () => {
     setEditItemIdx(-1);
-    setInputField("")
-  }
+    setInputField("");
+  };
+
+  useEffect(() => {
+    const localStorageData = localStorage.getItem("my-to-do-items");
+    if (localStorageData) {
+      setTodoItems(JSON.parse(localStorageData));
+    } else {
+      localStorage.setItem("my-to-do-items", []);
+    }
+  }, []);
+
+  // SAVE to localStorage
+  useEffect(() => {
+    console.log("todoitems updated");
+    localStorage.setItem("my-to-do-items", JSON.stringify(todoItems));
+  }, [todoItems]);
 
   return (
     <div className="todo-container">
@@ -81,78 +104,90 @@ export default function ToDoList() {
           />
         </label>
         <button className="submit-btn" type="submit">
-          {editItemIdx===-1 ? (
+          {editItemIdx === -1 ? (
             <AiOutlineFileAdd style={{ color: "white" }} />
           ) : (
             <BiSave style={{ color: "white" }} />
           )}
         </button>
       </form>
-            {/* save changes in localStorage  */}
 
-            {/* <button onClick={()=>{
-              localStorage.setItem('toDoList',)
-            }}>Save in localStorage</button> */}
-
-      {/** list of to do items container */}
       <div className="todos">
         <span>The Todos</span>
         <div>
           <label>
-            Search: 
-          <input
-            value={searchField} //read from the state
-            onChange={(e) => setSearchField(e.target.value)} //  update the state
-          />
+            Search:
+            <input
+              value={searchField} //read from the state
+              onChange={(e) => setSearchField(e.target.value)} //  update the state
+            />
           </label>
-       
-           <button className="filter-btn" onClick={()=>{setIsCompleted('')}}>all</button>
-           <button className="filter-btn" onClick={()=>{setIsCompleted('completed')}}>completed</button>
-           <button className="filter-btn" onClick={()=>{setIsCompleted('inCompleted')}}>incompleted</button>
+          {["all", "completed", "inCompleted"].map((btnName) => {
+            return (
+              <button
+                className="filter-btn"
+                onClick={() => {
+                  setIsCompleted(btnName);
+                }}
+              >
+                {btnName}
+              </button>
+            );
+          })}
+
         </div>
-       
-        {todoItems.filter((item)=>{
-          //  return item.title.includes(searchField)
-           return isCompleted === 'completed' ? (item.isCompleted && item.title.includes(searchField))  :isCompleted === 'inCompleted' ? (!item.isCompleted && item.title.includes(searchField)) : (item.title.includes(searchField))
-          
-         
-        }).map((item, itemIdx) => {
-          return (
-            <div className="todo" key={itemIdx}>
-              <div>
-                <div
-                  className={item.isCompleted ? "completed" : ""}
-                  style={{ fontSize: "20px" }}
-                >
-                  {item.title}
+
+        {todoItems
+          .filter((item) => {
+            return isCompleted === "completed"
+              ? item.isCompleted && item.title.includes(searchField)
+              : isCompleted === "inCompleted"
+              ? !item.isCompleted && item.title.includes(searchField)
+              : item.title.includes(searchField);
+          })
+          .map((item) => {
+            return (
+              <div className="todo" key={item.id}>
+                <div>
+                  <div
+                    className={item.isCompleted ? "completed" : ""}
+                    style={{ fontSize: "20px" }}
+                  >
+                    {item.title}
+                  </div>
+                  <div
+                    style={{ fontSize: "16px" }}
+                  >{`created: ${item.createdDate}`}</div>
                 </div>
-                <div
-                  style={{ fontSize: "16px" }}
-                >{`created: ${item.createdDate}`}</div>
+
+                {editItemIdx !== item.id ? (
+                  <div className="todo-btns">
+                    <button onClick={() => complete(item.id)}>
+                      <AiOutlineCheckCircle
+                        style={item.isCompleted && { color: "green" }}
+                      />
+                    </button>
+
+                    <button onClick={() => editItem(item)}>
+                      <ImPencil style={{ color: "blue" }} />
+                    </button>
+
+                    <button onClick={() => deleteItem(item.id)}>
+                      <ImBin2 style={{ color: "red" }} />
+                    </button>
+                  </div>
+                ) : (
+                  <button
+                    onClick={() => {
+                      cancelEdit();
+                    }}
+                  >
+                    Cancel
+                  </button>
+                )}
               </div>
-
-              {editItemIdx !== itemIdx ? (
-                <div className="todo-btns">
-                  <button onClick={() => complete(itemIdx)}>
-                    <AiOutlineCheckCircle
-                      style={item.isCompleted && { color: "green" }}
-                    />
-                  </button>
-
-                  <button onClick={() => editItem(itemIdx)}>
-                    <ImPencil style={{ color: "blue" }} />
-                  </button>
-
-                  <button onClick={() => deleteItem(itemIdx)}>
-                    <ImBin2 style={{ color: "red" }} />
-                  </button>
-                </div>
-              ) : (
-                <button onClick={()=>{cancelEdit()}}>Cancel</button>
-              )}
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
